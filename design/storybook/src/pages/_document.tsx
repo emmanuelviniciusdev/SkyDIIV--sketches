1942688955d0530ba78673320718f67337b7ab5b
emmanuelviniciusdev <emmanuelviniciusdev@gmail.com>
2026-09-19 16:18:22 -0300

    fix(storybook): replace Storybook icon

--- design/storybook/src/pages/_document.tsx
+++ design/storybook/src/pages/_document.tsx
@@ -4,8 +4,10 @@ export default function Document() {
   return (
     <Html lang="en-US">
       <Head>
-        <link rel="icon" href="/icon.png" type="image/png" sizes="1024x1024" />
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.svg?v=4" type="image/svg+xml" />
+        <link rel="icon" href="/favicon.ico?v=4" sizes="any" />
+        <link rel="icon" href="/icon.png?v=4" type="image/png" sizes="1024x1024" />
+        <link rel="apple-touch-icon" href="/icon.png?v=4" />
       </Head>
       <body className="antialiased">
         <Main />
